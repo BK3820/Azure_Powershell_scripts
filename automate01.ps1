8f562650d00986a00418e07c944cbed45305e058
Balakumar Palanisamy <80500214+BK3820@users.noreply.github.com>
2025-01-31 15:16:50 -0500

    Update automate01.ps1

--- automate01.ps1
+++ automate01.ps1
@@ -2,11 +2,16 @@
 $resourceGroupName = "MyResourceGroup"
 $location = "EastUS"
 
-# Login to Azure (if not already logged in)
-Connect-AzAccount
+# Convert GitHub Secret JSON string into a PowerShell object
+$azureCredentials = ConvertFrom-Json -InputObject $env:AZURE_CREDENTIALS
+
+# Login to Azure using Service Principal
+Connect-AzAccount -ServicePrincipal `
+    -TenantId $azureCredentials.tenantId `
+    -ApplicationId $azureCredentials.clientId `
+    -CertificateThumbprint $azureCredentials.clientSecret
 
 # Create the Resource Group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-# Confirm creation
 Write-Output "Resource Group '$resourceGroupName' created successfully in '$location'."
