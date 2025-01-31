1f6b573cbb490f48face75f909e28c92547a894a
Balakumar Palanisamy <80500214+BK3820@users.noreply.github.com>
2025-01-31 15:06:18 -0500

    Update automate01.ps1

--- automate01.ps1
+++ automate01.ps1
@@ -2,6 +2,8 @@
 $resourceGroupName = "MyResourceGroup"
 $location = "EastUS"
 
+# Login to Azure (if not already logged in)
+Connect-AzAccount
 
 # Create the Resource Group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
